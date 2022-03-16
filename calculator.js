a3985113264229a044b098c079fb2baeaf71a29e
brennenmichael <kelyen@aol.com>
2022-03-16 14:40:35 -0400

    Fine tuned operations.

--- calculator.js
+++ calculator.js
@@ -47,55 +47,79 @@ document.addEventListener("keydown", (e) => {
   }
 });
 
+//update display
+function updateDisplay() {
+  if (currentNumber.textContent !== "") {
+    lastNumber.textContent = currentNumber.textContent;
+    currentNumber.textContent = "";
+  }
+}
+
 //base functions
 function add(num1, num2) {
   num1 = Number(num1);
   num2 = Number(num2);
-  return num1 + num2;
+  lastNumber.textContent = num1 + num2;
+  currentNumber.textContent = "";
 }
 
 function subtract(num1, num2) {
   num1 = Number(num1);
   num2 = Number(num2);
-  return num1 - num2;
+  lastNumber.textContent = num1 - num2;
+  currentNumber.textContent = "";
 }
 
 function multiply(num1, num2) {
   num1 = Number(num1);
   num2 = Number(num2);
-  return num1 * num2;
+  lastNumber.textContent = num1 * num2;
+  currentNumber.textContent = "";
 }
 
 function divide(num1, num2) {
   num1 = Number(num1);
   num2 = Number(num2);
-  return num1 / num2;
+  lastNumber.textContent = num1 / num2;
+  currentNumber.textContent = "";
 }
 
 document.addEventListener("click", (e) => {
   elem = e.target.textContent;
-  if (operators.includes(elem)) {
-    if (elem === "+") {
-      lastNumber.textContent = add(
-        lastNumber.textContent,
-        currentNumber.textContent
-      );
-    } else if (elem === "-") {
-      lastNumber.textContent = subtract(
-        lastNumber.textContent,
-        currentNumber.textContent
-      );
-    } else if (elem === "x") {
-      lastNumber.textContent = multiply(
-        lastNumber.textContent,
-        currentNumber.textContent
-      );
-    } else if (elem === "/") {
-      lastNumber.textContent = divide(
-        lastNumber.textContent,
-        currentNumber.textContent
-      );
-    }
-    currentNumber.textContent = "";
+  if (elem === "+") {
+    updateDisplay();
+    operationToPerform = "+"; //acts as a failsafe and allows user to "change their mind on operation"
+    document.addEventListener("click", (e) => {
+      if (e.target.textContent === "=" && operationToPerform === "+")
+        add(lastNumber.textContent, currentNumber.textContent);
+    });
+  } else if (elem === "-") {
+    updateDisplay();
+    operationToPerform = "-";
+    document.addEventListener("click", (e) => {
+      if (e.target.textContent === "=" && operationToPerform === "-") {
+        subtract(lastNumber.textContent, currentNumber.textContent);
+      }
+    });
+  } else if (elem === "x") {
+    updateDisplay();
+    operationToPerform = "x";
+    document.addEventListener("click", (e) => {
+      if (e.target.textContent === "=" && operationToPerform === "x") {
+        multiply(lastNumber.textContent, currentNumber.textContent);
+      }
+    });
+  } else if (elem === "/") {
+    updateDisplay();
+    operationToPerform = "/";
+    document.addEventListener("click", (e) => {
+      if (e.target.textContent === "=" && operationToPerform === "/") {
+        divide(lastNumber.textContent, currentNumber.textContent);
+      }
+    });
   }
+});
+
+document.addEventListener("click", () => {
+  console.log(lastNumber.textContent, currentNumber.textContent);
 });
