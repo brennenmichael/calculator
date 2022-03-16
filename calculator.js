df35a90aae94b9fa308f183bc88b7c9d5a9294ab
brennenmichael <kelyen@aol.com>
2022-03-16 00:46:20 -0400

    Started operators, need to fine tune.

--- calculator.js
+++ calculator.js
@@ -1,5 +1,6 @@
 currentNumber = document.querySelector(".current-number");
 lastNumber = document.querySelector(".last-number");
+operators = ["+", "-", "x", "/", "="];
 
 //adding numbers to currentNumber
 document.addEventListener("click", (e) => {
@@ -43,5 +44,58 @@ document.addEventListener("click", (e) => {
 document.addEventListener("keydown", (e) => {
   if (e.key === "Backspace") {
     currentNumber.textContent = currentNumber.textContent.slice(0, -1);
+  }
+});
+
+//base functions
+function add(num1, num2) {
+  num1 = Number(num1);
+  num2 = Number(num2);
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  num1 = Number(num1);
+  num2 = Number(num2);
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  num1 = Number(num1);
+  num2 = Number(num2);
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  num1 = Number(num1);
+  num2 = Number(num2);
+  return num1 / num2;
+}
+
+document.addEventListener("click", (e) => {
+  elem = e.target.textContent;
+  if (operators.includes(elem)) {
+    if (elem === "+") {
+      lastNumber.textContent = add(
+        lastNumber.textContent,
+        currentNumber.textContent
+      );
+    } else if (elem === "-") {
+      lastNumber.textContent = subtract(
+        lastNumber.textContent,
+        currentNumber.textContent
+      );
+    } else if (elem === "x") {
+      lastNumber.textContent = multiply(
+        lastNumber.textContent,
+        currentNumber.textContent
+      );
+    } else if (elem === "/") {
+      lastNumber.textContent = divide(
+        lastNumber.textContent,
+        currentNumber.textContent
+      );
+    }
+    currentNumber.textContent = "";
   }
 });
